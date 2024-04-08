c753533c721619858d2f6257d5d077def19a3cd3
Sanchit Sharma <sanchitjsharma2603@gmail.com>
2024-04-08 16:25:50 +0530

    route changed again

--- src/components/User/DailyReport/DailyReport.jsx
+++ src/components/User/DailyReport/DailyReport.jsx
@@ -9,8 +9,8 @@ import { months } from '../../Calender/calender.js';
 const DailyReport = () => {
 
   // const route = "http://192.168.1.6:5000";
-  const route = "https://lambent-gaufre-c69eec.netlify.app/";
-  // const route = "https://pdash-backend.onrender.com";
+  // const route = "https://lambent-gaufre-c69eec.netlify.app/";
+  const route = "https://pdash-backend.onrender.com";
 
   const { currUser } = useLoaderData();
   const [plantID, setPlantID] = useState("Plant0");
